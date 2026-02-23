7547bd662cac452e10acc50dd172db185bf3e2dc
GUK <qrapls159@naver.com>
2026-02-24 02:52:33 +0900

    add WordPress template tags to front-page.php and header.php

--- header.php
+++ header.php
@@ -10,8 +10,22 @@
 <body <?php body_class(); ?>>
 
     <header>
-        <h1>Clean Arichitecture</h1>
-        <nav>nav</nav>
+        <h1><?php the_title(); ?></h1>
+        <nav>
+            <?php $terms = get_terms([
+                'taxonomy' => 'layer',
+                'hide_empty' => false,
+                'orderby' => 'slug',
+                'order' => 'ASC'
+            ]); ?>
+            <!-- get_terms() = term 목록 -->
+            <!-- get_term_link() = 특정 term의 URL -->
+            <?php foreach ($terms as $term) : ?>
+                <a href="<?php echo get_term_link($term); ?>">
+                    <?php echo $term->name ?>
+                </a>
+            <?php endforeach; ?>
+        </nav>
     </header>
 
     <main>
